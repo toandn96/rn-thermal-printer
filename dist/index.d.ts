45204ed5e848b459825580ed62baf16966d455c8
toandn96 <toandn96@gmail.com>
2020-12-23 00:27:20 +0700

    add

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -46,5 +46,6 @@ export declare const NetPrinter: {
 export declare const NetPrinterEventEmitter: NativeEventEmitter;
 export declare enum RN_THERMAL_RECEIPT_PRINTER_EVENTS {
     EVENT_NET_PRINTER_SCANNED_SUCCESS = "scannerResolved",
+    EVENT_NET_PRINTER_SCANNING = "scannerRunning",
     EVENT_NET_PRINTER_SCANNED_ERROR = "registerError"
 }
